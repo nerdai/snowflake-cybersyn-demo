6d8a207913732649295a5a9ece63da7e8e3eef7c
Andrei Fajardo <andrei@nerdai.io>
2024-07-23 23:55:40 -0400

    3 col layout

--- snowflake_cybersyn_demo/apps/streamlit.py
+++ snowflake_cybersyn_demo/apps/streamlit.py
@@ -12,30 +12,36 @@ def _llama_index_stream_wrapper(
         yield chunk.delta
 
 
-st.title("ChatGPT-like clone")
-
 llm = OpenAI(model="gpt-4o-mini")
+st.set_page_config(layout="wide")
+st.title("Human In The Loop W/ LlamaAgents")
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+left, middle, right = st.columns([1, 2, 1], vertical_alignment="bottom")
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+with left:
+    task_input = st.text_input("Task input", placeholder="Enter a task input.")
 
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+with middle:
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-    with st.chat_message("assistant"):
-        stream = llm.stream_chat(
-            messages=[
-                ChatMessage(role=m["role"], content=m["content"])
-                for m in st.session_state.messages
-            ]
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("What is up?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            stream = llm.stream_chat(
+                messages=[
+                    ChatMessage(role=m["role"], content=m["content"])
+                    for m in st.session_state.messages
+                ]
+            )
+            response = st.write_stream(_llama_index_stream_wrapper(stream))
+        st.session_state.messages.append(
+            {"role": "assistant", "content": response}
         )
-        response = st.write_stream(_llama_index_stream_wrapper(stream))
-    st.session_state.messages.append(
-        {"role": "assistant", "content": response}
-    )
